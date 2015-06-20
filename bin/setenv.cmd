83058194db4af1199ef21eda3c2bfc714b751ec5
aquielnt-robproper <rob.proper@aquilent.com>
2015-06-20 09:39:10 -0400

    -

--- bin/setenv.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-REM -----------------------------------------------------------------------------------------------
-REM IF YOU MAKE CHANGES TO THE STRUCTURE OF THIS FILE ALSO UPDATE setenv-template.cmd
-REM -----------------------------------------------------------------------------------------------
-
-set BRANCH_HOME=%PROJECT_HOME%/branches/master
